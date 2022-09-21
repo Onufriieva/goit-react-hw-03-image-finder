d34f8cb5b24477e01c78cc3de37576e99d710595
Onufriieva <onufrieva.julia@gmail.com>
2022-09-21 14:14:04 +0300

    Add components

---
+++ src/components/imageGallery/ImageGallery.jsx
@@ -0,0 +1,11 @@
+import ImageGalleryItem from "imageGalleryItem/ImageGalleryItem";
+
+const ImageGallery = () => {
+    return(
+        <ul class="gallery">
+         <ImageGalleryItem/>
+        </ul>
+    )
+};
+
+export default ImageGallery;
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,13 @@
+
+
+const Modal = () => {
+    return(
+        <div class="overlay">
+          <div class="modal">
+            <img src="" alt="" />
+          </div>
+        </div>
+    )
+};
+
+export default Modal;
