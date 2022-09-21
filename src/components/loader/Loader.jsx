e380c5aa385b6b265fda2b966a64037533f67ae7
Onufriieva <onufrieva.julia@gmail.com>
2022-09-21 15:00:32 +0300

    Add loader

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,18 @@
+import { RotatingLines } from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+const Loader = () => {
+  return(
+    <div>
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
+  )
+}
+
+export default Loader;
